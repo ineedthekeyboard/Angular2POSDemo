56623560261266ff23cc440c6ab46381a14c9183
Ryan Carlton <rlc120304@gmail.com>
2016-11-15 20:58:43 -0500

    Made a temporary products ordered list

--- src/app/products.service.ts
+++ src/app/products.service.ts
@@ -6,15 +6,16 @@ import { Product } from "./product-model/product.model";
 @Injectable()
 export class ProductsService {
 
+  private orderedProducts: Product[] = [];
   private productUrl = 'app/products';
-  private orderProductsUrl = 'app/productsOrdered';
   private headers = new Headers({ 'Content-Type': 'application/json' });
+
   constructor(
     private http: Http
   ) { }
   create(product: Product): Promise<Product> {
     return this.http
-      .post(this.productUrl, JSON.stringify({ name: name }), { headers: this.headers })
+      .post(this.productUrl, JSON.stringify(product), { headers: this.headers })
       .toPromise()
       .then(res => res.json().data)
       .catch(this.handleError);
@@ -43,17 +44,11 @@ export class ProductsService {
       .catch(this.handleError);
   }
   createProductsToOrder(products: Product[]): Promise<Product[]> {
-    return this.http
-      .post(this.orderProductsUrl, JSON.stringify(products), { headers: this.headers })
-      .toPromise()
-      .then(res => res.json().data)
-      .catch(this.handleError);
+    this.orderedProducts = products;
+    return Promise.resolve(this.orderedProducts);
   }
   getProductsToOrder(): Promise<Product[]> {
-    return this.http.get(this.orderProductsUrl)
-      .toPromise()
-      .then(response => response.json().data[0] as Product[])
-      .catch(this.handleError);
+    return Promise.resolve(this.orderedProducts);
   }
   private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
